0508041b791b346e91dea66e8ac9713d94ec90b6
Angel <53478143+007sya@users.noreply.github.com>
2022-01-21 00:34:29 +0100

    ✨ backend: Update resource routes with new filters

--- backend/api/routes/resources.py
+++ backend/api/routes/resources.py
@@ -1,7 +1,7 @@
 import datetime
 
 import fastapi
-from sqlmodel import select, Session
+from sqlmodel import desc, or_, select, Session
 
 from backend import scraping, settings
 from backend.api.dependencies import db_session, search_model
@@ -16,10 +16,10 @@ def _search_is_outdated(search: Search) -> bool:
 
 
 def _insert_new_songs(songs: list[Song], session: Session):
-    slugs = [song.slug for song in songs]
-    present_slugs = session.exec(
-        select(Song.slug).where(Song.slug in slugs)
-    ).all()
+    slugs = [song.g_slug for song in songs]
+    present_slugs = set(
+        session.exec(select(Song.g_slug).where(Song.g_slug in slugs)).all()
+    )
     songs = [song for song in songs if song not in present_slugs]
     session.add_all(songs)
     session.commit()
@@ -30,16 +30,24 @@ ROUTE_SEARCH_SONGS = "/songs/search/"
 
 @router.get(ROUTE_SEARCH_SONGS, response_model=list[SongSearch])
 async def search_songs(
-    query: str, search: Search = search_model, session: Session = db_session
+    query: str,
+    search: Search = search_model,
+    session: Session = db_session,
 ):
     """Search songs by query."""
-    songs: list[SongSearch]
 
     if _search_is_outdated(search):
         songs = await scraping.search_songs(query)
         search.date = datetime.datetime.now()
         _insert_new_songs([Song(**song.dict()) for song in songs], session)
         return songs
-    # songs = session.exec(select(Song).where())
-    songs = []
+
+    songs = session.exec(
+        select(Song)
+        .where(
+            or_(Song.title.like(f"%{query}%"), Song.author.like(f"%{query}%"))
+        )
+        .order_by(desc(Song.g_views))
+        .limit(10)
+    ).all()
     return songs
