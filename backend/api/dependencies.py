2e5f3fd438d147cfb91033fef2c15b05b84fb141
Angel <53478143+007sya@users.noreply.github.com>
2022-02-05 23:51:06 +0100

    🔥 backend: Remove unused dependencies

--- backend/api/dependencies.py
+++ backend/api/dependencies.py
@@ -1,26 +1,6 @@
-import datetime
-
 from fastapi import Depends
 from sqlmodel import Session
 
 from backend import settings
-from backend.api.models import Search
 
 db_session: Session = Depends(settings.get_db_session)
-
-
-def _get_search_model(query: str, session: Session = db_session):
-    page = 1
-    search = session.get(Search, (query, page))
-    if search is None:
-        return Search(
-            query=query,
-            page=page,
-            date=datetime.datetime.now()
-            + settings.SEARCH_CACHE_EXPIRATION
-            + datetime.timedelta(seconds=1),
-        )
-    return search
-
-
-search_model: Search = Depends(_get_search_model)
