eae9232b6da7430f3f4af44a937d12dae50dbcae
Angel <53478143+007sya@users.noreply.github.com>
2022-02-06 00:09:09 +0100

    ✅ backend: Add very basic search_songs test

--- tests/test_routes_resources.py
+++ tests/test_routes_resources.py
@@ -1,10 +1,21 @@
+import pytest
 from fastapi.testclient import TestClient
 
+from backend.api.models import SongSearch
 from backend.api.routes.resources import ROUTE_SEARCH_SONGS
 
-_MOUNT = "api/resources/"
+_MOUNT = "/api/resources/"
+
+
+@pytest.fixture
+def client(base_client: TestClient):
+    base_client.base_url = base_client.base_url + _MOUNT
+    return base_client
 
 
 def test_search_songs(client: TestClient):
-    url_search = f"{_MOUNT}{ROUTE_SEARCH_SONGS}"
-    print(url_search)
+    resp = client.get(ROUTE_SEARCH_SONGS[1:], params={"query": "hola"})
+    assert resp.status_code == 200
+    songs = [SongSearch(**song) for song in resp.json()]
+    for song in songs:
+        assert len(set(song.__fields__) - set(SongSearch.__fields__)) == 0
