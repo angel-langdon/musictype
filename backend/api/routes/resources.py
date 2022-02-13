2db2ca204f124d4042dac1ef243341cb1dff14d2
Angel <53478143+007sya@users.noreply.github.com>
2022-02-13 17:21:47 +0100

    ✨ backend: Add GET song lyrics endpoint

--- backend/api/routes/resources.py
+++ backend/api/routes/resources.py
@@ -1,18 +1,49 @@
 import fastapi
+from sqlmodel import Session
 
-from backend import scraping
-from backend.api.models import SongSearch
+from backend import scraping, song_utils
+from backend.api.dependencies import db_session, song_model
+from backend.api.models import Song, SongSearch
+from backend.sqlmodel_fix import select
 
 router = fastapi.APIRouter(prefix="/resources")
 
 
-ROUTE_SEARCH_SONGS = "/songs/search"
+def _upsert_songs(songs: list[Song], session: Session):
+    present_songs = {
+        slug: song_id
+        for song_id, slug in session.exec(
+            select(Song.id, Song.g_slug).where(
+                Song.g_slug.in_([song.g_slug for song in songs])
+            )
+        ).all()
+    }
+
+    session.bulk_save_objects(
+        [song for song in songs if song.g_slug not in present_songs]
+    )
+    session.commit()
+    for song in songs:
+        song.id = present_songs.get(song.g_slug, song.id)
 
 
-@router.get(ROUTE_SEARCH_SONGS, response_model=list[SongSearch])
-def search_songs(
-    query: str,
-):
+@router.get("/songs/search", response_model=list[SongSearch])
+def search_songs(query: str, session: Session = db_session):
     """Search songs by query."""
 
-    return scraping.search_songs(query)
+    songs = scraping.search_songs(query)
+    _upsert_songs(songs, session)
+    return songs
+
+
+@router.get("/song/lyrics/{song_id}", response_model=str)
+def song_lyrics(song: Song = song_model, session: Session = db_session):
+    """Get song lyrics"""
+    if song.lyrics:
+        return song_utils.clean_lyrics(song.lyrics)
+
+    lyrics = scraping.get_song_lyrics_from_slug(song.g_slug)
+    song.lyrics = lyrics
+    session.add(song)
+    session.commit()
+    return song_utils.clean_lyrics(lyrics)
