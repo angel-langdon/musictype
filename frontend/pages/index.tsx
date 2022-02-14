090b0dd0e8ccf80c91f1539d919e15c4e8250c06
Angel <53478143+007sya@users.noreply.github.com>
2022-02-15 00:53:14 +0100

    💄 frontend: Minor App layout changes

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,16 +1,20 @@
 import Head from "next/head";
+import TypingTest from "components/TypingTest";
 
 export default function Home() {
   return (
-    <div>
+    <div className="h-full">
       <Head>
         <title>MusicType</title>
         <meta name="description" content="MusicType" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className="text-3xl text-white">MusicType</h1>
+      <main className="h-full flex flex-col items-center relative">
+        <header className="flex w-full items-center justify-center p-5 mb-5">
+          <h1 className="text-3xl text-zinc-300">MusicType</h1>
+        </header>
+        <TypingTest />
       </main>
     </div>
   );
