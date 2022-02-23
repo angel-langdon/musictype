3156e4b09b1827c5bc93d0b5316c948698b1e456
Angel <53478143+007sya@users.noreply.github.com>
2022-02-24 00:47:03 +0100

    🐛 backend: Solve empty Song.title bug

--- backend/scraping.py
+++ backend/scraping.py
@@ -6,6 +6,7 @@ import httpx
 from bs4 import BeautifulSoup
 
 from backend.api.models import Song
+from backend.song_utils import clean_text
 
 re_lyrics_container = re.compile("^Lyrics__Container")
 
@@ -37,11 +38,12 @@ def search_songs(query: str, page: int = 1) -> list[Song]:
     songs: list[Song] = []
     for song_dict in dic["response"]["sections"][0]["hits"]:
         hit = song_dict["result"]
-        author: str = hit["full_title"].replace(hit["title"], "")
+        title = clean_text(hit["title"])
+        author = clean_text(hit["artist_names"])
         songs.append(
             Song(
-                title=hit["full_title"].replace(author, ""),
-                author=author.strip().lstrip("by "),
+                title=title,
+                author=author,
                 g_views=hit.get("stats", dict()).get("pageviews", 0),
                 g_slug=hit["path"],
             )
