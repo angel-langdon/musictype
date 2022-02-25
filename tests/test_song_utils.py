598d49ffc3f79737d83da04e2eafdac4451a4aec
Angel <53478143+007sya@users.noreply.github.com>
2022-02-25 22:16:53 +0100

    ✅ backend: Add song utils tests

--- tests/test_song_utils.py
+++ tests/test_song_utils.py
@@ -1,8 +1,15 @@
-from backend.song_utils import clean_text
+from backend.song_utils import clean_lyrics, clean_text
 
 
 def test_clean_text():
-    text = "“”´`‘’—е…àëìôú"
-    real_text = '""' + "''''" + "-e...àëìôúñ"
-    for clean, real in zip(clean_text(text), real_text):
-        assert clean == real
+    assert clean_text("“”⁗") == '"""'
+    assert clean_text("´`‘’") == "''''"
+    assert clean_text("—е…") == "-e..."
+    assert clean_text("àëìôú") == "àëìôú"
+
+
+def test_clean_lyrics():
+    assert clean_lyrics("\n\na\n \na\n\n") == "a\na"
+    assert clean_lyrics("[a](a)b") == "b"
+    assert clean_lyrics("a , ab") == "a, ab"
+    assert clean_lyrics("\r\ra\r\ra\r\r") == "a\na"
