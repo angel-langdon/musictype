bdece3e8c25117c500fd671d594707a57d7673ca
Angel <53478143+007sya@users.noreply.github.com>
2022-02-26 15:08:36 +0100

    ✨ frontend: Add Song context to app

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,21 +1,24 @@
 import Head from "next/head";
 import TypingTest from "components/TypingTest";
+import SongProvider from "contexts/SongProvider";
 
 export default function Home() {
   return (
-    <div className="h-full overflow-hidden">
-      <Head>
-        <title>MusicType</title>
-        <meta name="description" content="MusicType" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <SongProvider>
+      <div className="h-full overflow-hidden">
+        <Head>
+          <title>MusicType</title>
+          <meta name="description" content="MusicType" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className="h-full flex flex-col items-center relative">
-        <header className="flex w-full items-center justify-center p-5 mb-5">
-          <h1 className="text-3xl text-zinc-300">MusicType</h1>
-        </header>
-        <TypingTest />
-      </main>
-    </div>
+        <main className="h-full flex flex-col items-center relative">
+          <header className="flex w-full items-center justify-center p-5 mb-5">
+            <h1 className="text-3xl text-zinc-300">MusicType</h1>
+          </header>
+          <TypingTest />
+        </main>
+      </div>
+    </SongProvider>
   );
 }
