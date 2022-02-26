0617ac4c47b581b5c4c521d2fbd81e1c395eb2ec
Angel <53478143+007sya@users.noreply.github.com>
2022-02-26 21:17:32 +0100

    ✨ frontend: Add textSize prop to LetterHeight component

--- frontend/components/LetterHeight.tsx
+++ frontend/components/LetterHeight.tsx
@@ -1,12 +1,13 @@
-import { Dispatch, SetStateAction, useRef } from "react";
+import { Dispatch, SetStateAction } from "react";
 
 interface LetterHeightProps {
   setHeigth: Dispatch<SetStateAction<number>>;
+  textSize: string;
 }
 export default function LetterHeight(props: LetterHeightProps) {
   return (
     <div
-      className="fixed"
+      className={`fixed ${props.textSize}`}
       style={{ left: -1000 }}
       ref={(ref) => ref && props.setHeigth(ref.clientHeight)}
     >
