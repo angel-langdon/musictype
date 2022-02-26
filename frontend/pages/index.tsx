456ea1507604b910cfe97eb8a678cde19cc6671d
Angel <53478143+007sya@users.noreply.github.com>
2022-02-26 18:02:18 +0100

    ♻️ frontend: Simplify index page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,24 +1,5 @@
-import Head from "next/head";
-import TypingTest from "components/TypingTest";
-import SongProvider from "contexts/SongProvider";
+import SongSelect from "components/SongSelect";
 
-export default function Home() {
-  return (
-    <SongProvider>
-      <div className="h-full overflow-hidden">
-        <Head>
-          <title>MusicType</title>
-          <meta name="description" content="MusicType" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-
-        <main className="h-full flex flex-col items-center relative">
-          <header className="flex w-full items-center justify-center p-5 mb-5">
-            <h1 className="text-3xl text-zinc-300">MusicType</h1>
-          </header>
-          <TypingTest />
-        </main>
-      </div>
-    </SongProvider>
-  );
+export default function Index() {
+  return <SongSelect />;
 }
