5acd3a402bed10d42033efd21ac6fee88bf6009a
Angel <53478143+007sya@users.noreply.github.com>
2022-02-26 15:08:15 +0100

    ✨ frontend: Add Song context

---
+++ frontend/contexts/SongProvider.tsx
@@ -0,0 +1,25 @@
+import { Song } from "api/models";
+import { createContext, ReactNode, useContext, useState } from "react";
+
+function useSongState() {
+  const [song, setSong] = useState<Song | null>(null);
+
+  return {
+    song,
+    setSong,
+  };
+}
+
+type ISongContext = ReturnType<typeof useSongState>;
+const SongContext = createContext<ISongContext>(null!);
+
+export default function SongProvider({ children }: { children: ReactNode }) {
+  const songState = useSongState();
+  return (
+    <SongContext.Provider value={songState}>{children}</SongContext.Provider>
+  );
+}
+
+export function useSongContext(): ISongContext {
+  return useContext(SongContext);
+}
