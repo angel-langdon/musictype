9b48946d0131e2fdae4a309ccc1e11bde9ef50ff
Angel <53478143+007sya@users.noreply.github.com>
2022-03-02 00:27:35 +0100

    ✨ frontend: Add hook to pre-compute optimal TypingTest container width and height

--- frontend/components/TypingTest/TypingTest.tsx
+++ frontend/components/TypingTest/TypingTest.tsx
@@ -6,35 +6,53 @@ import Letter from "./Letter";
 
 interface TypingTestProps {
   song: Song;
-  letterHeight: number;
+  letter: { height: number; width: number };
   textSize: string;
 }
 
-const TypingTest = memo(({ song, letterHeight, textSize }: TypingTestProps) => {
+function useContainerSize(
+  lines: string[],
+  letter: TypingTestProps["letter"]
+): { width: string; height: number } {
+  return useMemo(() => {
+    if (lines.length === 0) return { width: "0px", height: 0 };
+    let sum = 0;
+    lines.forEach((line) => (sum += line.length));
+    const meanLength = sum / lines.length;
+    return {
+      width: `min(${(meanLength + 10) * letter.width}px, 80vw)`,
+      height: letter.height * 10,
+    };
+  }, [lines, letter]);
+}
+
+const TypingTest = memo(({ song, letter, textSize }: TypingTestProps) => {
   const ref = useRef<HTMLInputElement>(null);
   const [value, setValue] = useState("");
   const lines = useMemo(() => song.lyrics.split("\n"), [song.lyrics]);
+  const { width, height } = useContainerSize(lines, letter);
   let letterIdx = -1;
   return (
-    <div className="flex flex-col gap-y-4">
+    <div className="flex flex-col gap-y-5" style={{ width }}>
       <div>
-        {song.title} - {song.author}
+        {song.title} | {song.author}
       </div>
       <div
-        className="w-[60vw] overflow-scroll"
-        style={{ height: letterHeight * 10 }}
+        className="overflow-scroll"
         onClick={() => ref.current && ref.current.focus()}
+        style={{ height }}
       >
         <div className="flex flex-col flex-grow">
           {lines.map((line, line_idx) => (
             <div className={`${textSize} flex flex-wrap`} key={line_idx}>
-              {`${line} `.split("").map((letter) => {
+              {Array.from(`${line} `).map((letter) => {
                 letterIdx++;
                 return (
                   <Letter
                     key={letterIdx}
                     songLetter={letter}
                     writtenLetter={value[letterIdx]}
+                    isLetterActive={value.length === letterIdx}
                   />
                 );
               })}
