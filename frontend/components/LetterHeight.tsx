ca4cb3eb2b24174efb0271135f4aec757e6e9f71
Angel <53478143+007sya@users.noreply.github.com>
2022-03-01 23:26:59 +0100

    ✨ frontend: Add width to LetterHeight component

--- frontend/components/LetterHeight.tsx
+++ frontend/components/LetterHeight.tsx
@@ -1,15 +1,26 @@
+import { useRef } from "react";
+import { useEffect } from "react";
 import { Dispatch, SetStateAction } from "react";
 
 interface LetterHeightProps {
-  setHeigth: Dispatch<SetStateAction<number>>;
+  setLetter: Dispatch<SetStateAction<{ width: number; height: number }>>;
   textSize: string;
 }
 export default function LetterHeight(props: LetterHeightProps) {
+  const ref = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (!ref.current) return;
+    props.setLetter({
+      height: ref.current.clientHeight,
+      width: ref.current.clientWidth,
+    });
+  }, [ref]);
+
   return (
     <div
       className={`fixed ${props.textSize}`}
       style={{ left: -1000 }}
-      ref={(ref) => ref && props.setHeigth(ref.clientHeight)}
+      ref={ref}
     >
       A
     </div>
