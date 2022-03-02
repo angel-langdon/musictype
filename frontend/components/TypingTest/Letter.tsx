38601151b6e688657f12dc98b08ee6d4c7312c46
Angel <53478143+007sya@users.noreply.github.com>
2022-03-02 23:53:16 +0100

    ✨ frontend: Use isLetterActive prop in Letter component

--- frontend/components/TypingTest/Letter.tsx
+++ frontend/components/TypingTest/Letter.tsx
@@ -6,15 +6,14 @@ const wrongColor = "text-red-500";
 interface LetterProps {
   songLetter: string;
   writtenLetter: string | undefined;
+  isLetterActive: boolean;
 }
-
 function getProps(songLt: string, writtenLt: string | undefined) {
   const text = songLt === " " ? "\u00A0" : songLt;
   const isLetterUsed = writtenLt !== undefined;
   if (!isLetterUsed) return [text, ""];
   if (songLt === writtenLt) return [text, correctColor];
   if (songLt === " ") return [writtenLt, wrongColor];
-  if (songLt.normalize("NFD").includes(writtenLt)) return [text, correctColor];
   return [text, wrongColor];
 }
 
@@ -23,9 +22,9 @@ const Letter = memo((props: LetterProps) => {
   const [text, textColor] = getProps(props.songLetter, props.writtenLetter);
 
   useEffect(() => {
-    if (!ref.current || props.writtenLetter === undefined) return;
+    if (!ref.current || !props.isLetterActive) return;
     ref.current.scrollIntoView();
-  }, [props.songLetter, props.writtenLetter]);
+  }, [ref, props.isLetterActive]);
 
   return (
     <div ref={ref} className={textColor}>
