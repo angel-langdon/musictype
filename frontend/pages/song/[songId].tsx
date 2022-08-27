dd5a3f466f5b53e57b190526f2b75aa37d5ec285
Angel Langdon Villamayor <53478143+angel-langdon@users.noreply.github.com>
2022-08-27 14:54:42 +0200

    ✨ frontend: Add descriptive messages when response fails

--- frontend/pages/song/[songId].tsx
+++ frontend/pages/song/[songId].tsx
@@ -6,18 +6,19 @@ import { Song } from "api/models";
 import { doApiCall } from "api/client";
 import LetterSize from "components/LetterSize";
 
-async function loadSong(
-  songId: string,
-  setSong: Dispatch<SetStateAction<Song | null>>
-) {
-  const [res] = await doApiCall("GET", "resources", `song/${songId}`);
-  if (!res || res.status !== 200) return;
-  const song: Song | any = res.data;
-  setSong(song);
+async function getSong(songId: string) {
+  const [res, err] = await doApiCall("GET", "resources", `song/${songId}`);
+  let code = 0;
+  if (err?.response !== undefined) code = err.response.status;
+  else if (res?.status) code = res.status;
+  if (!res || res.status !== 200) return { song: null, code };
+  const song = res.data as Song;
+  return { song, code };
 }
 
 export default function SongTypingTest() {
   const [song, setSong] = useState<Song | null>(null);
+  const [code, setCode] = useState<null | number>(null);
   const router = useRouter();
   const { songId } = router.query;
   const [letter, setLetter] = useState({ height: -1, width: -1 });
@@ -25,9 +26,21 @@ export default function SongTypingTest() {
 
   useEffect(() => {
     if (typeof songId !== "string") return;
-    loadSong(songId, setSong);
-  }, [songId, setSong]);
-
+    getSong(songId).then(({ song, code }) => {
+      setSong(song);
+      setCode(code);
+    });
+  }, [songId]);
+  if (code === 404)
+    return <div className="text-white text-xl">Song not found</div>;
+  if (code === 422)
+    return <div className="text-white text-xl">Invalid Song ID {songId}</div>;
+  if (code !== 200)
+    return (
+      <div className="text-white text-xl">
+        Something went wrong, please try again in some minutes
+      </div>
+    );
   return (
     <div className="flex flex-col items-center justify-center">
       {!song || letter.height === -1 || letter.width === -1 ? (
